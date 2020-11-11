9cbebc140b7ddae9347e17a6b4e90ce528f34dc6
Antonio9721 <antonio.cobaep17@gmail.com>
2020-11-11 01:52:34 -0600

    Nuevos directorios y archivos añadidos

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from '../../services/noticias.service';
+import { Article } from '../interfaces/Interfaces';
+
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
 
 @Component({
   selector: 'app-about',
@@ -6,10 +15,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+  noticias: Article[] = [];
+    constructor(private noticiasService: NoticiasService) {
+    }
 
-  constructor() { }
+    componentes: Componente[] = [
+
+      {
+        icon: 'help-circle-outline',
+        name: 'Información',
+        redirecTo: '/informacion'
+      },
+      {
+        icon: 'hand-right-outline',
+        name: 'Recomendaciones y opiniones',
+        redirecTo: '/opinion'
+      },
+      {
+          icon: 'share-outline',
+          name: 'Publicaciones',
+          redirecTo: '/publicacion'
+      },
+      {
+          icon: 'camera-outline',
+          name: 'Fotos',
+          redirecTo: '/fotos'
+      },
+      {
+          icon: 'people-outline',
+          name: 'Comunidad',
+          redirecTo: '/comunidad'
+      }
+    ];
 
   ngOnInit() {
+    this.noticiasService.getTopHeadLines().subscribe(
+      resp => {console.log('noticias', resp);
+               this.noticias.push(...resp.articles);
   }
+    );
+}
 
 }
--- src/app/pages/publicacion/publicacion.page.ts
+++ src/app/pages/publicacion/publicacion.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NoticiasService } from '../../services/noticias.service';
+import { Article } from '../interfaces/Interfaces';
+
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
 
 @Component({
   selector: 'app-publicacion',
   templateUrl: './publicacion.page.html',
   styleUrls: ['./publicacion.page.scss'],
 })
-export class PublicacionPage implements OnInit {
 
-  constructor() { }
+export class PublicacionPage implements OnInit {
+  noticias: Article[] = [];
+    constructor(private noticiasService: NoticiasService) {
+    }
+
+
 
   ngOnInit() {
+    this.noticiasService.getTopHeadLines().subscribe(
+      resp => {console.log('noticias', resp);
+               this.noticias.push(...resp.articles);
+    
   }
+    );
+}
 
 }
